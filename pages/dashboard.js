cfe5ce342826e580840b502bccdd0284210f9034
Grixi <43199888+Grixi@users.noreply.github.com>
2021-04-04 22:56:09 +0200

    data done

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,27 @@
 import Head from "next/head";
+import DashboardShell from "@/components/DashboardShell";
+import { mutate } from "swr";
 
 import { useAuth } from "@/lib/auth";
 import EmptyState from "@/components/EmptyState";
-
+import useSWR from "swr";
+import fetcher from "@/utils/fetcher";
+import SiteTable from "@/components/SiteTable";
+import SiteTableSkeleton from "@/components/SiteTableSkeleton";
 const Dashboard = () => {
-	const auth = useAuth();
+	const { data } = useSWR("/api/sites", fetcher);
 
-	if (!auth.user) {
-		return "Loading...";
+	if (!data) {
+		<DashboardShell>
+			<SiteTableSkeleton />
+		</DashboardShell>;
 	}
 
-	return <EmptyState />;
+	return (
+		<DashboardShell>
+			{data?.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+		</DashboardShell>
+	);
 };
 
 export default Dashboard;
