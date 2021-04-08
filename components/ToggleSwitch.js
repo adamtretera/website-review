54c6a79526d7fd3e0333b7776fcdde299d7894e5
Grixi <43199888+Grixi@users.noreply.github.com>
2021-04-08 20:53:08 +0200

    test

--- components/ToggleSwitch.js
+++ components/ToggleSwitch.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const ToggleSwitch = () => {
 	return (
-		<div class="flex justify-between items-center" onClick={""}>
+		<div className="flex justify-between items-center" onClick={""}>
 			<h2>Toggle me</h2>
 			<div className="w-16 h-10 flex items-center bg-gray-300 rounded-full p-1 duration-300 ease-in-out">
-				<div class="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out"></div>
+				<div className="bg-white w-8 h-8 rounded-full shadow-md transform duration-300 ease-in-out"></div>
 			</div>
 		</div>
 	);
