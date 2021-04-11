553acc7ceceea02c056889d1c00dbf84cf1f6141
Grixi <43199888+Grixi@users.noreply.github.com>
2021-04-11 12:04:59 +0200

    stripe almost done backend

--- pages/account.js
+++ pages/account.js
@@ -23,7 +23,7 @@ const Account = () => {
 				>
 					{" "}
 					{isCheckoutLoading ? (
-						<div className="flex">
+						<div className="flex items-center">
 							<svg
 								xmlns="http://www.w3.org/2000/svg"
 								width="24"
@@ -52,9 +52,30 @@ const Account = () => {
 							<p>Načíta se</p>
 						</div>
 					) : (
-						<p>Upgradovat plán na startovač</p>
+						<div className="flex items-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								className="h-5 w-5 mr-3 fill-current"
+							>
+								<path d="M3.414,13.778L2,15.192l4.949,2.121l2.122,4.95l1.414-1.414l-0.707-3.536l3.313-3.313l3.61,7.704l1.339-1.339l-1.19-10.123 l2.828-2.829c0.781-0.781,0.781-2.047,0-2.828c-0.781-0.781-2.048-0.781-2.828,0l-2.903,2.903L3.824,6.297L2.559,7.563l7.644,3.67 l-3.253,3.253L3.414,13.778z"></path>
+							</svg>
+							<p>Upgradovat plán na startovač</p>
+						</div>
 					)}
 				</button>
+				<button
+					className=" bg-white text-black active:bg-pink-600 border-2 border-black dark:border-white dark:bg-black dark:text-white px-6 py-3  outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+					onClick={() => {
+						setBillingLoading(true);
+						goToBillingPortal();
+					}}
+				>
+					View Billing Portal
+				</button>
+				<button onClick={() => signout()}>Log Out</button>
 			</Box>
 		</DashboardShell>
 	);
