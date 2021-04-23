6d079bd5673e00cf657d9363288fe440bc82caf3
Grixi <43199888+Grixi@users.noreply.github.com>
2021-04-23 22:06:27 +0200

    love W

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,55 +1,28 @@
-import { useRef, useState } from "react";
-import { useRouter } from "next/router";
-import { getAllFeedback, getAllSites, getSite } from "@/lib/db-admin";
-import Feedback from "@/components/Feedback";
-import FeedbackLink from "@/components/FeedbackLink";
+import { auth } from "@/lib/firebase-admin";
+import { getUserSites } from "@/lib/db-admin";
+import { logger, formatObjectKeys } from "@/utils/logger";
 
-export async function getStaticProps(context) {
-	const [siteId, route] = context.params.site;
-	const { feedback } = await getAllFeedback(siteId, route);
-	const { site } = await getSite(siteId);
-	return {
-		props: {
-			initialFeedback: feedback,
-			site,
-		},
-		revalidate: 1,
-	};
-}
-export async function getStaticPaths() {
-	const { sites } = await getAllSites();
-	const paths = sites.map((site) => ({
-		params: {
-			site: [site.id.toString()],
-		},
-	}));
+export default async (req, res) => {
+	try {
+		const { uid } = await auth.verifyIdToken(req.headers.token);
+		const { sites } = await getUserSites(uid);
 
-	return {
-		paths,
-		fallback: true,
-	};
-}
+		res.status(200).json({ sites });
+	} catch (error) {
+		logger.error(
+			{
+				request: {
+					headers: formatObjectKeys(req.headers),
+					url: req.url,
+					method: req.method,
+				},
+				response: {
+					statusCode: res.statusCode,
+				},
+			},
+			error.message
+		);
 
-const EmbeddedFeedbackPage = ({ initialFeedback, site }) => {
-	const router = useRouter();
-
-	return (
-		<div>
-			<FeedbackLink paths={router.query.site} />
-			{initialFeedback?.length ? (
-				initialFeedback.map((feedback, index) => (
-					<Feedback
-						key={feedback.id}
-						settings={site?.settings}
-						isLast={index === initialFeedback.length - 1}
-						{...feedback}
-					/>
-				))
-			) : (
-				<div>Nejsou tu žadné feedbaky na tuto stránku.</div>
-			)}
-		</div>
-	);
+		res.status(500).json({ error });
+	}
 };
-
-export default EmbeddedFeedbackPage;
--- pages/site/[...site].js
+++ pages/site/[...site].js
@@ -1,24 +1,17 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/router";
-
-import { useAuth } from "@/lib/auth";
-import { createFeedback } from "@/lib/db";
 import { getAllFeedback, getAllSites, getSite } from "@/lib/db-admin";
 import Feedback from "@/components/Feedback";
-import DashboardShell from "@/components/DashboardShell";
-import SiteHeader from "@/components/SiteHeader";
-import { useForm } from "react-hook-form";
-import LoginButtons from "@/components/LoginButtons";
+import FeedbackLink from "@/components/FeedbackLink";
 
 export async function getStaticProps(context) {
 	const [siteId, route] = context.params.site;
 	const { feedback } = await getAllFeedback(siteId, route);
 	const { site } = await getSite(siteId);
-
 	return {
 		props: {
 			initialFeedback: feedback,
-			site: site,
+			site,
 		},
 		revalidate: 1,
 	};
@@ -37,73 +30,26 @@ export async function getStaticPaths() {
 	};
 }
 
-const FeedbackPage = ({ initialFeedback, site }) => {
-	const { user, loading } = useAuth();
-
+const EmbeddedFeedbackPage = ({ initialFeedback, site }) => {
 	const router = useRouter();
 
-	const [siteId, route] = router.query.site;
-
-	const initialRef = useRef(null);
-	const {
-		handleSubmit,
-		register,
-		formState: { errors },
-	} = useForm();
-	const onCreateFeedback = ({ feedback }) => {
-		const newFeedback = {
-			siteId,
-		};
-		createFeedback(newFeedback);
-		setAllFeedback([newFeedback, ...allFeedback]);
-	};
-
-	const LoginOrLeaveFeedback = () =>
-		user ? (
-			<button
-				type="submit"
-				className="bg-white text-black active:bg-pink-600 border-2 border-black dark:border-white dark:bg-black dark:text-white px-6 py-3  outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-			>
-				Zanechat feedback
-			</button>
-		) : (
-			<LoginButtons />
-		);
-
 	return (
-		<DashboardShell>
-			<SiteHeader
-				isSiteOwner={true}
-				site={site}
-				siteId={siteId}
-				route={route}
-			/>
-
-			{initialFeedback &&
+		<div>
+			<FeedbackLink paths={router.query.site} />
+			{initialFeedback?.length ? (
 				initialFeedback.map((feedback, index) => (
-					<Feedback key={feedback.id} settings={site?.settings} {...feedback} />
-				))}
-			<form onSubmit={handleSubmit(onCreateFeedback)}>
-				<label className="block pt-4 pb-2 text-xl">Nějaký ten feedback</label>
-
-				<input
-					className="border-2 text-sm bg-white  border-black dark:border-white dark:bg-black w-full py-2 px-4 "
-					ref={initialRef}
-					disabled={router.isFallback}
-					placeholder="Tato stránka se mi moc libí, jenom bych uprail..."
-					name="feedback"
-					{...register("feedback", { required: true })}
-				></input>
-				{errors.site && (
-					<span>Jestli chceš dát feedback, toto pole je povinné.</span>
-				)}
-
-				<div className="flex items-center justify-end pt-6">
-					{!loading && <LoginOrLeaveFeedback />}
-				</div>
-			</form>
-		</DashboardShell>
+					<Feedback
+						key={feedback.id}
+						settings={site?.settings}
+						isLast={index === initialFeedback.length - 1}
+						{...feedback}
+					/>
+				))
+			) : (
+				<div>Nejsou tu žadné feedbaky na tuto stránku.</div>
+			)}
+		</div>
 	);
 };
 
-export default FeedbackPage;
+export default EmbeddedFeedbackPage;
